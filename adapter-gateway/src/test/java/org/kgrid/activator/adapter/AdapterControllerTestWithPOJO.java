c6bd459e58f06324c4e8fb7c4e9c1391ce98d0ed
pboisver <pboisver@umich.edu>
2018-03-13 14:17:31 -0400

    Remove uneeded framework tests

--- adapter-gateway/src/test/java/org/kgrid/activator/adapter/AdapterControllerTestWithPOJO.java
+++ adapter-gateway/src/test/java/org/kgrid/activator/adapter/AdapterControllerTestWithPOJO.java
@@ -1,8 +1,5 @@
 package org.kgrid.activator.adapter;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kgrid.activator.adapter.api.Adapter;
@@ -25,76 +22,5 @@ public class AdapterControllerTestWithPOJO {
   @Test
   public void contextLoads() {
   }
-
-  @Test
-  public void testStatusWithPOJO() throws Exception {
-
-    AdapterController ac2 = new AdapterController(adapter);
-
-    when(adapter.supports("whatever")).thenReturn(true);
-
-    assertEquals("UP", ac2.status());
-
-    when(adapter.supports("whatever")).thenReturn(false);
-
-    assertEquals("DOWN", ac2.status());
-  }
-
-  @Test
-  public void testStatusWithMVC() throws Exception {
-    AdapterController ac2 = new AdapterController(adapter);
-
-    when(adapter.supports("whatever")).thenReturn(true);
-
-    assertEquals(true, adapter.supports("whatever"));
-
-    assertEquals("UP", ac2.status());
-
-    when(adapter.supports("whatever")).thenReturn(false);
-
-    assertEquals(false, adapter.supports("whatever"));
-
-    assertEquals("DOWN", ac2.status());
-//        given(adapter.supports(anyString())).willReturn(true); // if using mock adapter
-
-//		ResultActions response = mockMvc.perform(get("/status"))
-//				.andExpect(status().isOk())
-//				.andExpect(content().string("UP"))
-//				;
-  }
-
-
-  enum Types {FOO, BAR;}
-
-//    @TestConfiguration
-//    public static class AdapterTestContextConfiguration {
-//
-//
-//        @Bean
-//        Adapter adapter() {
-//            return new Adapter() {
-//
-//                @Override
-//                public boolean supports(String typeName) {
-//                    return true;
-//                }
-//
-//                @Override
-//                public EnumSet<Types> getTypes() {
-//                    return EnumSet.allOf(Types.class);
-//                }
-//
-//                @Override
-//                public void initialize() {
-//
-//                }
-//
-//                @Override
-//                public Executor activate(CompoundKnowledgeObject spec) {
-//                    return null;
-//                }
-//            };
-//        }
-//    }
 
 }
